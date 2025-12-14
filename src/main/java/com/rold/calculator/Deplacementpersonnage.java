d216872fdbfb3c9af2c29e8555cf7d5bc5f6b583
darhill <rolddarhill18@gmail.com>
2025-12-14 18:37:24 +0100

    test green

--- src/main/java/com/rold/calculator/Deplacementpersonnage.java
+++ src/main/java/com/rold/calculator/Deplacementpersonnage.java
@@ -1,6 +1,10 @@
 package com.rold.calculator;
 
 public class Deplacementpersonnage {
-
+      private int orientation=0;
+      public String getorientation()
+      {
+            return "NORD";
+      }
 
 }
--- src/test/java/com/rold/calculator/DeplacementpersonnageTest.java
+++ src/test/java/com/rold/calculator/DeplacementpersonnageTest.java
@@ -1,16 +1,12 @@
 package com.rold.calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DeplacementpersonnageTest {
-   @Test
-    void orientation_initial_au_nord()
-   {
-       Deplacementpersonnage p = new Deplacementpersonnage();
-       asserThat(p.getorientation()).isEqualTo("NORD");
-   }
-
+    @Test
+    void orientation_initial_au_nord() {
+        Deplacementpersonnage p = new Deplacementpersonnage();
+        assertThat(p.getorientation()).isEqualTo("NORD");
+    }
 }
